c33f5754511b9b86936df40abbe2b53b65f65c6f
Douglas <eu@douglaspaz.com>
2018-02-22 22:16:12 -0300

    Add RepositoryExample and RepositoryExampleEntity in bothub.common

--- bothub/common/models.py
+++ bothub/common/models.py
@@ -51,6 +51,59 @@ class RepositoryUpdate(models.Model):
         _('created at'),
         auto_now_add=True)
 
-class RepositoryUpdateIntent(models.Model):
-    # TODO
-    pass
+class RepositoryExample(models.Model):
+    class Meta:
+        verbose_name = _('repository example')
+        verbose_name_plural = _('repository examples')
+    
+    repository = models.ForeignKey(
+        RepositoryUpdate,
+        models.CASCADE,
+        editable=False)
+    repository_update = models.ForeignKey(
+        RepositoryUpdate,
+        models.CASCADE,
+        related_name='added',
+        blank=True,
+        null=True,
+        editable=False)
+    deleted_in = models.ForeignKey(
+        RepositoryUpdate,
+        models.CASCADE,
+        related_name='deleted',
+        blank=True,
+        null=True,
+        editable=False)
+    text = models.TextField(
+        _('text'),
+        editable=False)
+    intent = models.CharField(
+        _('intent'),
+        max_length=64,
+        blank=True,
+        editable=False)
+
+class RepositoryExampleEntity(models.Model):
+    class Meta:
+        verbose_name = _('repository example entity')
+        verbose_name_plural = _('repository example entities')
+    
+    repository_example = models.ForeignKey(
+        RepositoryExample,
+        models.CASCADE,
+        related_name='entities',
+        editable=False)
+    start = models.PositiveIntegerField(
+        _('start'),
+        editable=False)
+    end = models.PositiveIntegerField(
+        _('end'),
+        editable=False)
+    entity = models.CharField(
+        _('entity'),
+        max_length=64,
+        editable=False)
+
+    @property
+    def value(self):
+        return self.repository_example.text[self.start:self.end]
