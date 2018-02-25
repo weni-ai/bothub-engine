78c4e00ca3ed115e91d66fd501bba84e1ff6992a
Douglas Paz <eu@douglaspaz.com>
2018-02-25 20:45:19 -0300

    Add api/reposiroty/[uuid] router

--- bothub/api/routers.py
+++ bothub/api/routers.py
@@ -2,7 +2,9 @@ from rest_framework import routers
 
 from .views import NewRepositoryViewSet
 from .views import MyRepositoriesViewSet
+from .views import RepositoryViewSet
 
 router = routers.SimpleRouter()
 router.register('repository/new', NewRepositoryViewSet)
 router.register('myrepositories', MyRepositoriesViewSet)
+router.register('repository', RepositoryViewSet)
--- bothub/api/views.py
+++ bothub/api/views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
 from .serializers import RepositorySerializer
 from bothub.common.models import Repository
 
 
-class NewRepositoryViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+# Permisions
+
+class IsRepositoryOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+# ViewSets
+
+class NewRepositoryViewSet(
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet):
     queryset = Repository.objects
     serializer_class = RepositorySerializer
-    permission_classes = [IsAuthenticated]
-
+    permission_classes = [permissions.IsAuthenticated]
 
 class MyRepositoriesViewSet(
     mixins.ListModelMixin,
     viewsets.GenericViewSet):
     queryset = Repository.objects
     serializer_class = RepositorySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self, *args, **kwargs):
         return self.queryset.filter(owner=self.request.user)
+
+class RepositoryViewSet(
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet):
+    queryset = Repository.objects
+    serializer_class = RepositorySerializer
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsRepositoryOwner,
+    ]
