e87cf41b1a9fdc0f5b2f4faeebe7dcd667013910
Douglas Paz <eu@douglaspaz.com>
2018-02-25 20:23:02 -0300

    Add api/myrepositories router

--- bothub/api/routers.py
+++ bothub/api/routers.py
@@ -1,6 +1,8 @@
 from rest_framework import routers
 
 from .views import NewRepositoryViewSet
+from .views import MyRepositoriesViewSet
 
 router = routers.SimpleRouter()
 router.register('repository/new', NewRepositoryViewSet)
+router.register('myrepositories', MyRepositoriesViewSet)
--- bothub/api/views.py
+++ bothub/api/views.py
@@ -6,7 +6,19 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import RepositorySerializer
 from bothub.common.models import Repository
 
+
 class NewRepositoryViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Repository.objects
     serializer_class = RepositorySerializer
     permission_classes = [IsAuthenticated]
+
+
+class MyRepositoriesViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet):
+    queryset = Repository.objects
+    serializer_class = RepositorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self, *args, **kwargs):
+        return self.queryset.filter(owner=self.request.user)
