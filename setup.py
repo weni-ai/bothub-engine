77b879d52d6a4815017e16273fce18e5417841ff
Douglas Paz <eu@douglaspaz.com>
2018-02-27 19:31:09 -0300

    Init pip package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+setup(
+    name='bothub-app',
+    version='0.1.0',
+    description='bothub app',
+    packages=find_packages(),
+)
