1587d3cad1ad100a68a3b481abf7c982b2e375e0
Douglas Paz <eu@douglaspaz.com>
2018-02-27 00:10:13 -0300

    Enable api pagination

--- bothub/settings.py
+++ bothub/settings.py
@@ -120,5 +120,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
 }
