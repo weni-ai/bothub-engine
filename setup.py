9d7a8eb8898a9c2c8bbf496374817c876f3c7247
Douglas Paz <eu@douglaspaz.com>
2018-02-27 22:21:38 -0300

    Fix requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 from os import path
 
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+install_requires = filter(lambda x: len(x) > 0, install_requires.split('\n'))
 
 setup(
     name='bothub-app',
-    version='0.1.0',
+    version='0.1.1',
     description='bothub app',
     packages=find_packages(),
+    install_requires=install_requires,
 )
