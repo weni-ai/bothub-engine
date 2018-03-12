e1fb9910c0db3c01f0cbbee4b0997ef420770369
Douglas Paz <eu@douglaspaz.com>
2018-03-12 13:46:03 -0300

    Add RepositoryTranslatedExampleRetrieveTestCase in api tests

--- bothub/api/tests/translate.py
+++ bothub/api/tests/translate.py
@@ -10,6 +10,7 @@ from bothub.common.models import RepositoryExample
 from bothub.common.models import RepositoryTranslatedExample
 
 from ..views import NewRepositoryTranslatedExampleViewSet
+from ..views import RepositoryTranslatedExampleViewSet
 
 from .utils import create_user_and_token
 
@@ -77,3 +78,60 @@ class TranslateExampleTestCase(TestCase):
         self.assertIn(
             'non_field_errors',
             content_data.keys())
+
+
+class RepositoryTranslatedExampleRetrieveTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.owner, self.owner_token = create_user_and_token('owner')
+
+        self.repository = Repository.objects.create(
+            owner=self.owner,
+            name='Testing',
+            slug='test',
+            language=languages.LANGUAGE_EN)
+        self.example = RepositoryExample.objects.create(
+            repository_update=self.repository.current_update(),
+            text='hi')
+        self.translated = RepositoryTranslatedExample.objects.create(
+            original_example=self.example,
+            language=languages.LANGUAGE_PT,
+            text='oi')
+
+        self.private_repository = Repository.objects.create(
+            owner=self.owner,
+            name='Private',
+            slug='private',
+            language=languages.LANGUAGE_EN)
+        self.private_example = RepositoryExample.objects.create(
+            repository_update=self.private_repository.current_update(),
+            text='hi')
+        self.private_translated = RepositoryTranslatedExample.objects.create(
+            original_example=self.private_example,
+            language=languages.LANGUAGE_PT,
+            text='oi')
+
+    def request(self, translated, token):
+        authorization_header = {
+            'HTTP_AUTHORIZATION': 'Token {}'.format(token.key),
+        }
+        request = self.factory.get(
+            '/api/translated/{}/'.format(translated.id),
+            **authorization_header)
+        response = RepositoryTranslatedExampleViewSet.as_view(
+            {'get': 'retrieve'})(request, pk=translated.id)
+        response.render()
+        content_data = json.loads(response.content)
+        return (response, content_data,)
+
+    def test_okay(self):
+        response, content_data = self.request(
+            self.translated,
+            self.owner_token)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK)
+        self.assertEqual(
+            content_data.get('id'),
+            self.translated.id)
