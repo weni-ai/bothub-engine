96e496d5bf0c18935fda46ded2b758f49f5ff048
Douglas Paz <eu@douglaspaz.com>
2018-03-13 15:43:26 -0300

    Add check password in RegisterUserTestCase

--- bothub/api/tests/user.py
+++ bothub/api/tests/user.py
@@ -5,6 +5,8 @@ from django.test import RequestFactory
 from django.test.client import MULTIPART_CONTENT
 from rest_framework import status
 
+from bothub.authentication.models import User
+
 from ..views import RegisterUserViewSet
 from ..views import UserViewSet
 
@@ -26,15 +28,19 @@ class RegisterUserTestCase(TestCase):
         return (response, content_data,)
 
     def test_okay(self):
+        email = 'fake@user.com'
+        password = 'abc!1234'
         response, content_data = self.request({
-            'email': 'fake@user.com',
+            'email': email,
             'name': 'Fake',
             'nick': 'fake',
-            'password': 'abc!1234',
+            'password': password,
         })
         self.assertEqual(
             response.status_code,
             status.HTTP_201_CREATED)
+        user = User.objects.get(email=email)
+        self.assertTrue(user.check_password(password))
 
     def test_invalid_password(self):
         response, content_data = self.request({
