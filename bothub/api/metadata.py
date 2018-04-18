7b582b931f8bbdce57b07ae06d482e1ed203e723
Douglas Paz <eu@douglaspaz.com>
2018-04-18 09:12:26 -0300

    Add custom DRF metadata class

---
+++ bothub/api/metadata.py
@@ -0,0 +1,18 @@
+from django.utils.encoding import force_text
+from rest_framework.metadata import SimpleMetadata
+
+from .fields import ModelMultipleChoiceField
+
+
+class Metadata(SimpleMetadata):
+    def get_field_info(self, field):
+        field_info = super().get_field_info(field)
+        if isinstance(field, ModelMultipleChoiceField):
+            field_info['choices'] = [
+                {
+                    'value': choice_value,
+                    'display_name': force_text(choice_name, strings_only=True)
+                }
+                for choice_value, choice_name in field.choices.items()
+            ]
+        return field_info
--- bothub/settings.py
+++ bothub/settings.py
@@ -143,6 +143,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_METADATA_CLASS': 'bothub.api.metadata.Metadata',
 }
 
 
