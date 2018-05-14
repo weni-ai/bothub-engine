c7978c4cc94c1959d39a5a1c6d026179819d0167
Douglas Paz <eu@douglaspaz.com>
2018-05-14 10:14:20 -0300

    Remove unused permission

--- bothub/api/views.py
+++ bothub/api/views.py
@@ -66,15 +66,6 @@ class RepositoryExamplePermission(permissions.BasePermission):
         return authorization.can_contribute
 
 
-class RepositoryExampleEntityPermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        authorization = obj.repository_example.repository_update.repository \
-            .get_user_authorization(request.user)
-        if request.method in READ_METHODS:
-            return authorization.can_read
-        return authorization.can_contribute
-
-
 class RepositoryTranslatedExamplePermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         repository = obj.original_example.repository_update.repository
