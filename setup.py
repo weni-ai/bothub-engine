e98dae93234b89c050d878749eecdd1b7d6b068d
Douglas Paz <eu@douglaspaz.com>
2018-05-18 15:26:42 -0300

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = list(
 
 setup(
     name='bothub',
-    version='1.3.0',
+    version='1.4.0',
     description='bothub',
     packages=find_packages(),
     install_requires=install_requires,
