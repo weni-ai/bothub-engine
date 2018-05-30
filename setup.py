742e2c7047fa44699ac400e755810ce8eb3314fb
Douglas Paz <eu@douglaspaz.com>
2018-05-30 10:38:44 -0300

    Bump version to 1.7.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = list(
 
 setup(
     name='bothub',
-    version='1.7.0',
+    version='1.7.1',
     description='bothub',
     packages=find_packages(),
     install_requires=install_requires,
