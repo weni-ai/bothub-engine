d320211974cc131d3ae773f109ace1c81cee7d60
Douglas Paz <eu@douglaspaz.com>
2018-06-11 10:02:21 -0300

    Bump version to 1.8.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = list(
 
 setup(
     name='bothub',
-    version='1.8.7',
+    version='1.8.8',
     description='bothub',
     packages=find_packages(),
     install_requires=install_requires,
