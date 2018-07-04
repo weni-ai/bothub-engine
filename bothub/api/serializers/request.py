1adbfd4e07075a2f5fa5ae31df2d03a2b0c95e68
Douglas Paz <eu@douglaspaz.com>
2018-07-04 15:16:59 -0300

    Update NewRequestRepositoryAuthorizationSerializer apresentation

--- bothub/api/serializers/request.py
+++ bothub/api/serializers/request.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 from bothub.common.models import RequestRepositoryAuthorization
+from bothub.common.models import Repository
+from ..fields import TextField
 
 
 class NewRequestRepositoryAuthorizationSerializer(serializers.ModelSerializer):
@@ -12,10 +15,18 @@ class NewRequestRepositoryAuthorizationSerializer(serializers.ModelSerializer):
             'text',
         ]
 
+    repository = serializers.PrimaryKeyRelatedField(
+        queryset=Repository.objects,
+        style={'show': False})
     user = serializers.PrimaryKeyRelatedField(
         read_only=True,
         default=serializers.CurrentUserDefault(),
         style={'show': False})
+    text = TextField(
+        label=_('Leave a message for repository administrators'),
+        min_length=5,
+        max_length=RequestRepositoryAuthorization._meta.get_field(
+            'text').max_length)
 
 
 class RequestRepositoryAuthorizationSerializer(serializers.ModelSerializer):
