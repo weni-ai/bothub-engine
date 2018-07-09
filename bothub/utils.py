8aa9c37a76f082b9534bf6e61df840b5ef56b978
Douglas Paz <eu@douglaspaz.com>
2018-07-09 16:43:29 -0300

    Add cast_supported_languages

---
+++ bothub/utils.py
@@ -0,0 +1,8 @@
+from collections import OrderedDict
+
+
+def cast_supported_languages(i):
+    return OrderedDict([
+        x.split(':', 1) if ':' in x else (x, x) for x in
+        i.split('|')
+    ])
