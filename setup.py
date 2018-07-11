42720b5134add44b8eacebb36ce21427f2b40cca
Douglas Paz <eu@douglaspaz.com>
2018-07-11 09:33:16 -0300

    Bump version to 1.13.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bothub',
-    version='1.13.6',
+    version='1.13.7',
     description='bothub',
     packages=find_packages(),
     install_requires=[
