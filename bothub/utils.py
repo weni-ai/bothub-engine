9088cd345237e8f266feb048a35a0bd81277534d
Douglas Paz <eu@douglaspaz.com>
2018-07-12 17:51:24 -0300

    Update EMAIL_HOST and CSRF_COOKIE_DOMAIN cast in settings

--- bothub/utils.py
+++ bothub/utils.py
@@ -6,3 +6,7 @@ def cast_supported_languages(i):
         x.split(':', 1) if ':' in x else (x, x) for x in
         i.split('|')
     ])
+
+
+def cast_empty_str_to_none(value):
+    return value or None
