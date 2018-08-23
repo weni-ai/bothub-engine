3a23efc3700d168328253f3a2e1561f2bfd2735b
Douglas Paz <eu@douglaspaz.com>
2018-08-23 15:53:12 -0300

    Add repository shortcut

---
+++ bothub/api/v2/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from . import views
+from .routers import router
+
+
+urlpatterns = [
+    path(
+        'repository-shortcut/<slug:owner__nickname>/<slug:slug>/',
+        views.repository_shortcut,
+        name='repository-shortcut'),
+    path('', include(router.urls)),
+]
--- bothub/urls.py
+++ bothub/urls.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from rest_framework.documentation import include_docs_urls
 
 from bothub.api.v1.routers import router as bothub_api_routers
-from bothub.api.v2.routers import router as bothub_api_v2_routers
+from bothub.api.v2 import urls as bothub_api_v2_urls
 from bothub.health.views import ping
 
 
 urlpatterns = [
     path('api/', include(bothub_api_routers.urls)),
-    path('api/v2/', include(bothub_api_v2_routers.urls)),
+    path('api/v2/', include(bothub_api_v2_urls)),
     path('docs/', include_docs_urls(title='API Documentation')),
     path('admin/', admin.site.urls),
     path('ping/', ping, name='ping'),
