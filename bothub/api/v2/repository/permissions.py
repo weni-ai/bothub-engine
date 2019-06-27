a2aaa03f98cabb2ca8f995d8a56aa7249a5f4f49
Daniel Yohan <dyohan9@gmail.com>
2019-06-27 11:17:44 -0300

    [fix] Permissions repository

--- bothub/api/v2/repository/permissions.py
+++ bothub/api/v2/repository/permissions.py
@@ -7,9 +7,12 @@ from .. import WRITE_METHODS
 class RepositoryPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         authorization = obj.get_user_authorization(request.user)
-        if request.method in READ_METHODS:
+        if request.method in READ_METHODS and not request.user.is_authenticated:
             return authorization.can_read
+
         if request.user.is_authenticated:
+            if request.method in READ_METHODS:
+                return authorization.can_read
             if request.method in WRITE_METHODS:
                 return authorization.can_write
             return authorization.is_admin
