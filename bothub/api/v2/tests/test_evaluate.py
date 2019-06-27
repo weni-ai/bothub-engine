52aad6357edb5aad291326165f44a30fc59367bf
Daniel Yohan <dyohan9@gmail.com>
2019-06-27 11:37:59 -0300

    Added test update evaluate

--- bothub/api/v2/tests/test_evaluate.py
+++ bothub/api/v2/tests/test_evaluate.py
@@ -180,3 +180,82 @@ class EvaluateDestroyTestCase(TestCase):
         self.assertIsNotNone(
             self.repository_evaluate.deleted_in
         )
+
+
+class EvaluateUpdateTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.owner, self.owner_token = create_user_and_token('owner')
+        self.user, self.token = create_user_and_token()
+
+        self.repository = Repository.objects.create(
+            owner=self.owner,
+            name='Testing',
+            slug='test',
+            language=languages.LANGUAGE_EN
+        )
+
+        self.repository_update = RepositoryUpdate.objects.create(
+            repository=self.repository,
+            language='en',
+            algorithm='statistical_model',
+        )
+
+        self.example_1 = RepositoryExample.objects.create(
+            repository_update=self.repository_update,
+            text="test",
+            intent="greet",
+        )
+
+        self.repository_evaluate = RepositoryEvaluate.objects.create(
+            repository_update=self.repository_update,
+            text="test",
+            intent="greet"
+        )
+
+    def request(self, data, token):
+        authorization_header = {
+            'HTTP_AUTHORIZATION': 'Token {}'.format(token.key),
+        }
+        request = self.factory.patch(
+            '/api/v2/evaluate/{}/?repository_uuid={}'.format(self.repository_evaluate.id, self.repository.uuid),
+            json.dumps(data),
+            content_type='application/json',
+            **authorization_header)
+        response = EvaluateViewSet.as_view(
+            {'patch': 'update'})(request, pk=self.repository_evaluate.id, repository_uuid=self.repository.uuid)
+        response.render()
+        content_data = json.loads(response.content)
+
+        return (response, content_data,)
+
+    def test_okay(self):
+        text = 'testing'
+        response, content_data = self.request(
+            {
+                'repository': str(self.repository.uuid),
+                'text': text,
+                'language': languages.LANGUAGE_EN,
+                'intent': 'greet',
+                'entities': []
+            }, self.owner_token
+        )
+        self.assertEqual(content_data['text'], text)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK)
+
+    def test_private_okay(self):
+        response, content_data = self.request(
+            {
+                'repository': str(self.repository.uuid),
+                'text': 'testing',
+                'language': languages.LANGUAGE_EN,
+                'intent': 'greet',
+                'entities': []
+            }, self.token)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_403_FORBIDDEN)
+
