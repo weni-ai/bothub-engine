0587d04e515e5230cccb3ab885495550c0b3caea
Daniel Yohan <dyohan9@gmail.com>
2019-08-16 17:50:24 -0300

    [fix] Migrations

--- bothub/common/migrations/0033_auto_20190816_2030.py
+++ bothub/common/migrations/0033_auto_20190816_2030.py
@@ -11,6 +11,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RemoveField(
+            model_name='repositoryvote',
+            name='vote',
+        ),
         migrations.AddField(
             model_name='repositoryvote',
             name='created',
