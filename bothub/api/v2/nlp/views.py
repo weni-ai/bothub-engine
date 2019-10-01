f2d7cb4f83694fa9a360a1381b7698aa73e2b257
Daniel Yohan <dyohan9@gmail.com>
2019-10-01 10:00:31 -0300

    Updated imports

--- bothub/api/v2/nlp/views.py
+++ bothub/api/v2/nlp/views.py
@@ -4,7 +4,6 @@ import re
 import requests
 
 from django.conf import settings
-
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import get_object_or_404
