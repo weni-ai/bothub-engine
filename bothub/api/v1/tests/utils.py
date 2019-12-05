857d5c279ce538f6facbeed5be8ca2233ad9c197
Daniel Yohan <dyohan9@gmail.com>
2019-12-04 23:24:38 -0300

    Removed v1

--- bothub/api/v1/tests/utils.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.authtoken.models import Token
-
-from bothub.authentication.models import User
-
-
-def create_user_and_token(nickname="fake"):
-    user = User.objects.create_user("{}@user.com".format(nickname), nickname)
-    token, create = Token.objects.get_or_create(user=user)
-    return (user, token)
