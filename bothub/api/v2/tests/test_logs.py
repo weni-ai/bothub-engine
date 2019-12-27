20754aa8e9e9aa311be7662b938286720fabb2c4
Daniel Yohan <dyohan9@gmail.com>
2019-12-27 16:10:52 -0300

    Updated test

--- bothub/api/v2/tests/test_logs.py
+++ bothub/api/v2/tests/test_logs.py
@@ -8,7 +8,12 @@ from bothub.api.v2.nlp.views import RepositoryNLPLogsViewSet
 from bothub.api.v2.repository.views import RepositoryNLPLogViewSet
 from bothub.api.v2.tests.utils import create_user_and_token
 from bothub.common import languages
-from bothub.common.models import Repository, RepositoryAuthorization, RepositoryNLPLog, RepositoryNLPLogIntent
+from bothub.common.models import (
+    Repository,
+    RepositoryAuthorization,
+    RepositoryNLPLog,
+    RepositoryNLPLogIntent,
+)
 from bothub.common.models import RepositoryExample
 
 
@@ -34,60 +39,48 @@ class RepositoryNLPLogTestCase(TestCase):
 
     def request(self, data):
         request = self.factory.post(
-            "/v2/repository/nlp/log/",
-            json.dumps(data),
-            content_type="application/json"
+            "/v2/repository/nlp/log/", json.dumps(data), content_type="application/json"
         )
-        response = RepositoryNLPLogsViewSet.as_view({"post": "create"})(
-            request
-        )
+        response = RepositoryNLPLogsViewSet.as_view({"post": "create"})(request)
         response.render()
         content_data = json.loads(response.content)
         return (response, content_data)
 
     def test_okay(self):
         data = {
-                "text": 'test',
-                "user_agent": 'python-requests/2.20.1',
-                "user": str(self.repository_auth.pk),
-                "repository_version_language": int(self.repository.current_version().pk),
-                "nlp_log": json.dumps(
-                    {
-                        "intent": {
-                            "name": "bias",
-                            "confidence": 0.9994810819625854
-                        },
-                        "intent_ranking": [{
-                            "name": "bias",
-                            "confidence": 0.9994810819625854
-                        }, {
-                            "name": "doubt",
-                            "confidence": 0.039212167263031006
-                        }, {
-                            "name": "negative",
-                            "confidence": 0.0
-                        }, {
-                            "name": "affirmative",
-                            "confidence": 0.0
-                        }],
-                        "labels_list": [],
-                        "entities_list": [],
-                        "entities": {},
-                        "text": "test",
-                        "repository_version": int(self.repository.current_version().pk),
-                        "language": str(self.repository.language)
-                    }
-                ),
-                "log_intent": [],
-            }
+            "text": "test",
+            "user_agent": "python-requests/2.20.1",
+            "user": str(self.repository_auth.pk),
+            "repository_version_language": int(self.repository.current_version().pk),
+            "nlp_log": json.dumps(
+                {
+                    "intent": {"name": "bias", "confidence": 0.9994810819625854},
+                    "intent_ranking": [
+                        {"name": "bias", "confidence": 0.9994810819625854},
+                        {"name": "doubt", "confidence": 0.039212167263031006},
+                        {"name": "negative", "confidence": 0.0},
+                        {"name": "affirmative", "confidence": 0.0},
+                    ],
+                    "labels_list": [],
+                    "entities_list": [],
+                    "entities": {},
+                    "text": "test",
+                    "repository_version": int(self.repository.current_version().pk),
+                    "language": str(self.repository.language),
+                }
+            ),
+            "log_intent": [],
+        }
         response, content_data = self.request(data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertEqual(data.get('user_agent'), content_data.get('user_agent'))
-        self.assertEqual(data.get('text'), content_data.get('text'))
-        self.assertEqual(data.get('nlp_log'), content_data.get('nlp_log'))
-        self.assertEqual(data.get('repository_version'), content_data.get('repository_version'))
-        self.assertEqual(data.get('language'), content_data.get('language'))
+        self.assertEqual(data.get("user_agent"), content_data.get("user_agent"))
+        self.assertEqual(data.get("text"), content_data.get("text"))
+        self.assertEqual(data.get("nlp_log"), content_data.get("nlp_log"))
+        self.assertEqual(
+            data.get("repository_version"), content_data.get("repository_version")
+        )
+        self.assertEqual(data.get("language"), content_data.get("language"))
 
 
 class ListRepositoryNLPLogTestCase(TestCase):
@@ -107,74 +100,62 @@ class ListRepositoryNLPLogTestCase(TestCase):
         )
 
         nlp_log = RepositoryNLPLog.objects.create(
-            text='test',
-            user_agent='python-requests/2.20.1',
+            text="test",
+            user_agent="python-requests/2.20.1",
             repository_version_language=self.repository.current_version(),
             nlp_log=json.dumps(
-                    {
-                        "intent": {
-                            "name": "bias",
-                            "confidence": 0.9994810819625854
-                        },
-                        "intent_ranking": [{
-                            "name": "bias",
-                            "confidence": 0.9994810819625854
-                        }, {
-                            "name": "doubt",
-                            "confidence": 0.039212167263031006
-                        }, {
-                            "name": "negative",
-                            "confidence": 0.0
-                        }, {
-                            "name": "affirmative",
-                            "confidence": 0.0
-                        }],
-                        "labels_list": [],
-                        "entities_list": [],
-                        "entities": {},
-                        "text": "test",
-                        "repository_version": int(self.repository.current_version().pk),
-                        "language": str(self.repository.language)
-                    }
-                ),
+                {
+                    "intent": {"name": "bias", "confidence": 0.9994810819625854},
+                    "intent_ranking": [
+                        {"name": "bias", "confidence": 0.9994810819625854},
+                        {"name": "doubt", "confidence": 0.039212167263031006},
+                        {"name": "negative", "confidence": 0.0},
+                        {"name": "affirmative", "confidence": 0.0},
+                    ],
+                    "labels_list": [],
+                    "entities_list": [],
+                    "entities": {},
+                    "text": "test",
+                    "repository_version": int(self.repository.current_version().pk),
+                    "language": str(self.repository.language),
+                }
+            ),
             user=self.owner,
         )
 
         RepositoryNLPLogIntent.objects.create(
-            intent='bias',
+            intent="bias",
             confidence=0.9994810819625854,
             is_default=True,
-            repository_nlp_log=nlp_log
+            repository_nlp_log=nlp_log,
         )
 
         RepositoryNLPLogIntent.objects.create(
-            intent='doubt',
+            intent="doubt",
             confidence=0.039212167263031006,
             is_default=False,
-            repository_nlp_log=nlp_log
+            repository_nlp_log=nlp_log,
         )
 
         RepositoryNLPLogIntent.objects.create(
-            intent='negative',
+            intent="negative",
             confidence=0.0,
             is_default=False,
-            repository_nlp_log=nlp_log
+            repository_nlp_log=nlp_log,
         )
 
         RepositoryNLPLogIntent.objects.create(
-            intent='affirmative',
+            intent="affirmative",
             confidence=0.0,
             is_default=False,
-            repository_nlp_log=nlp_log
+            repository_nlp_log=nlp_log,
         )
 
     def request(self, data, token=None):
         authorization_header = (
             {"HTTP_AUTHORIZATION": "Token {}".format(token.key)} if token else {}
         )
-        request = self.factory.get(
-            "/v2/repository/log/", data, **authorization_header
-        )
+        request = self.factory.get("/v2/repository/log/", data, **authorization_header)
         response = RepositoryNLPLogViewSet.as_view({"get": "list"})(request)
         response.render()
         content_data = json.loads(response.content)
@@ -186,4 +167,4 @@ class ListRepositoryNLPLogTestCase(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(content_data.get("count"), 1)
-        self.assertEqual(len(content_data.get("results")[0].get('log_intent')), 4)
+        self.assertEqual(len(content_data.get("results")[0].get("log_intent")), 4)
