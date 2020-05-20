78f5be17970d6dc296e23febe5b110a65e8b3ef9
Daniel Yohan <dyohan9@gmail.com>
2020-05-20 12:34:49 -0300

    Custom Migration

--- bothub/common/migrations/0057_migrate_labels.py
+++ bothub/common/migrations/0057_migrate_labels.py
@@ -6,11 +6,16 @@ def noop(apps, schema_editor):  # pragma: no cover
 
 
 def migrate(apps, schema_editor):  # pragma: no cover
-    Repository = apps.get_model("common", "Repository")
+    RepositoryVersion = apps.get_model("common", "RepositoryVersion")
     RepositoryEntityLabel = apps.get_model("common", "RepositoryEntityLabel")
+    RepositoryEntityGroup = apps.get_model("common", "RepositoryEntityGroup")
 
-    for repo in Repository.objects.all():
-        print(repo)
+    for repository_version in RepositoryVersion.objects.all():
+        print(repository_version.repository)
+        old_labels = RepositoryEntityLabel.objects.filter(repository=repository_version.repository)
+        print(old_labels)
+        for label in old_labels:
+            print(label.value)
 
 
 class Migration(migrations.Migration):
