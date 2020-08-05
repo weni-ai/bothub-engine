0020df710df895c50582e169e681cde3b7af1cd2
Daniel Yohan <dyohan9@gmail.com>
2020-08-05 14:13:16 -0300

    Updated django admin

--- bothub/common/views.py
+++ bothub/common/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponseRedirect
 from django.core.exceptions import ValidationError
 from django.contrib.admin.views.decorators import staff_member_required
 
-from bothub.common.models import RepositoryVersion
+from bothub.common.models import RepositoryNLPTrain
 
 
 @staff_member_required
 def download_bot_data(self, update_id):  # pragma: no cover
-    update = get_object_or_404(RepositoryVersion, pk=update_id)
-    if not update.trained_at:
+    update = get_object_or_404(RepositoryNLPTrain, pk=update_id)
+    if update.bot_data is None or update.bot_data == "":
         raise ValidationError(f"Update #{update.pk} not trained at.")
-    response = HttpResponseRedirect(update.get_bot_data())
+    response = HttpResponseRedirect(
+        update.repositoryversionlanguage.get_bot_data.bot_data
+    )
     return response
