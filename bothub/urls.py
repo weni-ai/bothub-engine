ca3ea2cd9c3a46143f43195180d7f60a3466c876
Daniel Yohan <dyohan9@gmail.com>
2020-09-14 19:03:25 -0300

    Update urls.py

--- bothub/urls.py
+++ bothub/urls.py
@@ -16,7 +16,7 @@ from bothub.common.views import download_bot_data
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
-        default_version="v2.1.2",
+        default_version="v2.1.3",
         description="Documentation",
         terms_of_service="https://bothub.it/terms",
         contact=openapi.Contact(email="bothub@ilhasoft.com.br"),
