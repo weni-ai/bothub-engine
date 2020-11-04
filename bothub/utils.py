13518e835b28b958fcc60a32f05301a8764521ab
Daniel Yohan <dyohan9@gmail.com>
2020-11-04 11:59:09 -0300

    Added support for apm elastic observability

--- bothub/utils.py
+++ bothub/utils.py
@@ -290,9 +290,7 @@ def request_nlp(auth, nlp_server, route, data):
     try:
         url = f"{nlp_server if nlp_server else settings.BOTHUB_NLP_BASE_URL}"
         url += f"{route}/?"
-        header = {
-            "Authorization": "Bearer " + auth
-        }
+        header = {"Authorization": "Bearer " + auth}
         r = requests.post(url, json=data, headers=header)
         data = r.json()
         return data
