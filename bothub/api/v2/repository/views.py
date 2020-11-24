9fb36c98975032323a03758a75efb0263f10911a
markirius <markbombomb@gmail.com>
2020-11-24 17:41:41 -0300

    fix trailling whitespace

--- bothub/api/v2/repository/views.py
+++ bothub/api/v2/repository/views.py
@@ -419,9 +419,9 @@ class RepositoryViewSet(
             user_authorization, serializer.data
         )
         if request.status_code != status.HTTP_200_OK:  # pragma: no cover
-            raise APIException(  # pragma: no cover
+            raise APIException(
                 {"status_code": request.status_code}, code=request.status_code
-            )
+            )  # pragma: no cover
         return Response(request.json())  # pragma: no cover
 
     @action(
@@ -439,12 +439,11 @@ class RepositoryViewSet(
         user_authorization = repository.get_user_authorization(request.user)
         if not user_authorization.can_write:
             raise PermissionDenied()
-        serializer = EvaluateSerializer(data=request.data)  
+        serializer = EvaluateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         task = celery_app.send_task(
-            name="auto_evaluate",
-            args=[serializer.data, str(user_authorization)]
+            name="auto_evaluate", args=[serializer.data, str(user_authorization)]
         )
         task.wait()
         return Response(task.result)
