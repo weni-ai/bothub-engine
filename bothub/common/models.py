42c597d7da4cc7352ff5717dc9f455c34e12248d
markirius <markbombomb@gmail.com>
2020-11-25 11:28:18 -0300

    added new type choice for automatic evaluate in RepositoryQueueTask

--- bothub/common/models.py
+++ bothub/common/models.py
@@ -1189,9 +1189,11 @@ class RepositoryQueueTask(models.Model):
     ]
     TYPE_PROCESSING_TRAINING = 0
     TYPE_PROCESSING_AUTO_TRANSLATE = 1
+    TYPE_PROCESSING_AUTO_EVALUATE = 2
     TYPE_PROCESSING_CHOICES = [
         (TYPE_PROCESSING_TRAINING, _("NLP Tranining")),
         (TYPE_PROCESSING_AUTO_TRANSLATE, _("Repository Auto Translation")),
+        (TYPE_PROCESSING_AUTO_EVALUATE, _("Repository Auto Evaluate")),
     ]
 
     repositoryversionlanguage = models.ForeignKey(
