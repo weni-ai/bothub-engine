e6ae14851c6ffe15d38678cea224fcb2d2349282
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2021-04-01 12:18:31 -0300

    add language to user proto and serializer

--- bothub/api/grpc/user/serializers.py
+++ bothub/api/grpc/user/serializers.py
@@ -14,6 +14,7 @@ class UserProtoSerializer(proto_serializers.ModelProtoSerializer):
             "email",
             "nickname",
             "name",
+            "language",
             "joined_at",
             "is_active",
             "is_superuser",
