127c6654124b6e5757fefc1c070d890739fff49d
helllllllder <heldergsds@gmail.com>
2021-09-17 15:15:03 -0300

    fix sonarcloud

--- bothub/api/v2/nlp/views.py
+++ bothub/api/v2/nlp/views.py
@@ -69,10 +69,8 @@ class RepositoryAuthorizationTrainViewSet(
 
         repository_version = request.query_params.get("repository_version")
         if repository_version:
-            current_version = (
-                repository_authorization.repository.get_specific_version_id(
-                    repository_version, str(request.query_params.get("language"))
-                )
+            current_version = repository_authorization.repository.get_specific_version_id(
+                repository_version, str(request.query_params.get("language"))
             )
         else:
             current_version = repository_authorization.repository.current_version(
@@ -787,3 +785,4 @@ class RepositoryAuthorizationExamplesViewSet(mixins.RetrieveModelMixin, GenericV
             )
 
         return self.get_paginated_response(examples_return)
+        
