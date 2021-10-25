83e4ac5900434b89cc980091240c5dca5e72a905
helllllllder <heldergsds@gmail.com>
2021-10-25 18:12:06 -0300

    add condition to remove None values from the knowledge_bases_languages list

--- bothub/common/models.py
+++ bothub/common/models.py
@@ -601,7 +601,8 @@ class Repository(models.Model):
             )
         else:
             knowledge_bases_languages = list(set(self.knowledge_bases.values_list("texts__language", flat=True)))
-            knowledge_bases_languages.remove(None)
+            if None in knowledge_bases_languages:
+                knowledge_bases_languages.remove(None)
             return knowledge_bases_languages
 
     @property
