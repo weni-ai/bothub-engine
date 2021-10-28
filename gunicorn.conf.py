1076eaab059a19c2491deb0ea0fa95635c86047a
helllllllder <heldergsds@gmail.com>
2021-10-28 13:48:23 -0300

    uncomment bind and update workers at gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,6 @@ import multiprocessing
 
 
 # bind = '0.0.0.0:80'
-workers = os.environ.get('GUNICORN_WORKERS', None) or multiprocessing.cpu_count() * 2 + 1
+workers = os.environ.get('GUNICORN_WORKERS', multiprocessing.cpu_count() * 2 + 1)
 worker_class = 'gevent'
 raw_env = ['DJANGO_SETTINGS_MODULE=bothub.settings']
