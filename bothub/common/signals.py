2d717c6e9c98041b9f8188dce389f1c4588f19a9
helllllllder <heldergsds@gmail.com>
2021-11-08 12:38:26 -0300

    remove delete and pre_delete tasks and signals as they're doesn't work well with celery

--- bothub/common/signals.py
+++ bothub/common/signals.py
@@ -15,22 +15,3 @@ class CelerySignalProcessor(RealTimeSignalProcessor):
                     "es_handle_save", args=[instance.pk, app_label, model_name]
                 )
             )
-
-    def handle_pre_delete(self, sender, instance, **kwargs):
-        app_label = instance._meta.app_label
-        model_name = instance._meta.model_name
-        model = instance._meta.concrete_model
-        if model in registry._models or model in registry._related_models:
-            celery_app.send_task(
-                "es_handle_pre_delete", args=[instance.pk, app_label, model_name]
-            )
-
-    def handle_delete(self, sender, instance, **kwargs):
-        # app_label = instance._meta.app_label  // NOSONAR
-        # model_name = instance._meta.model_name  // NOSONAR
-        # model = instance._meta.concrete_model  // NOSONAR
-        # if model in registry._models or model in registry._related_models:  // NOSONAR
-        #     celery_app.send_task(  // NOSONAR
-        #         "es_handle_delete", args=[instance.pk, app_label, model_name]  // NOSONAR
-        #     )  // NOSONAR
-        pass
