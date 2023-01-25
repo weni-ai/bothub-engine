78d96d7f64a1c6513f57f590a1b47490a775ce92
Jackson Barbosa <jbs@ic.ufal.br>
2023-01-25 09:33:37 -0300

    adding middleware to get project info

--- bothub/api/v2/middleware.py
+++ bothub/api/v2/middleware.py
@@ -19,3 +19,43 @@ class UserLanguageMiddleware:
         response = self.get_response(request)
 
         return response
+
+
+class ProjectOrganizationMiddleware:
+    def __init__(self):
+        ...
+    
+    def __call__(self, project_uuid) -> Any:
+        from bothub.common.models import Project
+        from bothub.api.v2.internal.connect_rest_client import ConnectRESTClient
+        from bothub.utils import organization_unique_slug_generator
+        project_uuid = request.params.get("project_uuid")
+        project = Project.objects.filter(uuid=project_uuid)
+        
+        if project.exists():
+            project = project.first()
+            return project
+        connect_client = ConnectRESTClient()
+        response = connect_client.get_project_info(project_uuid=project_uuid)
+        if response.status_code == 200:
+            project_data = response.get("project")
+            
+            organization_data = response.get("organization")
+            authorizations = response.get("authorizations")
+            owner = response.get("owner")
+
+            formatted_organization_data = {
+                "name": organization_data.get("name"),
+                "nickname": organization_unique_slug_generator(
+                    organization_data.get("name")
+                ),
+                "description": organization_data.get("description", ""),
+                "locale": organization_data.get("locale", ""),
+            }
+            org = Organization.objects.create(**formatted_organization_data)
+
+            Project.objects.create(
+                uuid=project_data.get("uuid"),
+                name=project_data.get("name"),
+                organization=org
+            )
