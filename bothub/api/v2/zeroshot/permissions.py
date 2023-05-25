668cb695e0e86f0f18895058831dad1b6a4ccbbb
Jackson Barbosa <jbs@ic.ufal.br>
2023-05-25 15:47:47 -0300

    update zeroshot permissions

--- bothub/api/v2/zeroshot/permissions.py
+++ bothub/api/v2/zeroshot/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from bothub.common.models import RepositoryAuthorization
 from .. import READ_METHODS
 
 class ZeroshotOptionsPermission(permissions.BasePermission):
@@ -6,15 +7,22 @@ class ZeroshotOptionsPermission(permissions.BasePermission):
         authorization = obj.repository_zeroshot.repository.get_user_authorization(
             request.user
         )
+        print(request.data, request.user, request.method, authorization.role, authorization.can_read, authorization.can_contribute) 
         if request.method in READ_METHODS:
             return authorization.can_read
         return authorization.can_contribute
 
 class ZeroshotOptionsTextPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        authorization = obj.option.repository_zeroshot.repository.get_user_authorization(
-            request.user
-        )
+        print(request.data, request.user)
+        if request.user.is_authenticated:
+            authorization = obj.option.repository_zeroshot.repository.get_user_authorization(
+                request.user
+            )
+        else:
+            if "token" in request.data:
+                authorization = RepositoryAuthorization.objects.get(uuid=request.data.get("token"), repository=request.get("repository_uuid"))
+            
         if request.method in READ_METHODS:
             return authorization.can_read
         return authorization.can_contribute
