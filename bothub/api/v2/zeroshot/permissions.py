6897aafc21042f6f523b59df97b81217782e1710
Jackson Barbosa <jbs@ic.ufal.br>
2023-05-25 16:17:44 -0300

    adjust option permission

--- bothub/api/v2/zeroshot/permissions.py
+++ bothub/api/v2/zeroshot/permissions.py
@@ -4,9 +4,14 @@ from .. import READ_METHODS
 
 class ZeroshotOptionsPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        authorization = obj.repository_zeroshot.repository.get_user_authorization(
-            request.user
-        )
+        if request.user.is_authenticated:
+            authorization = obj.option.repository_zeroshot.repository.get_user_authorization(
+                request.user
+            )
+        else:
+            if "token" in request.data:
+                authorization = RepositoryAuthorization.objects.get(uuid=request.data.get("token"), repository=request.get("repository_uuid"))
+        
         print(request.data, request.user, request.method, authorization.role, authorization.can_read, authorization.can_contribute) 
         if request.method in READ_METHODS:
             return authorization.can_read
