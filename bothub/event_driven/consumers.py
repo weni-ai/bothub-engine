97628e4881e56e06862a3cc46c4d97f682ad72a8
Jackson Barbosa <jbs@ic.ufal.br>
2023-08-29 15:01:34 -0300

    base consumer code

---
+++ bothub/event_driven/consumers.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+import amqp
+
+from .signals import message_started, message_finished
+
+
+class EDAConsumer(ABC):  # pragma: no cover
+    def handle(self, message: amqp.Message):
+        message_started.send(sender=self)
+        try:
+            self.consume(message)
+        finally:
+            message_finished.send(sender=self)
+
+    @abstractmethod
+    def consume(self, message: amqp.Message):
+        pass
