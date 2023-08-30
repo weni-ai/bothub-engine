50755934fa2b106ff18434d9076ffb1a5244b1e7
Jackson Barbosa <jbs@ic.ufal.br>
2023-08-30 09:20:23 -0300

    adding base consume data

---
+++ bothub/event_driven/consumer/consumers.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+import amqp
+
+from bothub.event_driven.singnals import message_started, message_finished
+
+class EDAConsumer(ABC):  # pragma: no cover
+    def handle(self, message: amqp.Message):
+        message_started.send(sender=self)
+        try:
+            self.consume(message)
+        finally:
+            message_finished.send(sender=self)
+
+    @abstractmethod
+    def consume(self, message: amqp.Message):
+        pass
