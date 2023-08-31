1449499ab401ab619d4254e6ea99b5ab1e5b7767
Jackson Barbosa <jbs@ic.ufal.br>
2023-08-30 22:08:12 -0300

    fix creation project intelligence key

--- bothub/project/usecases/template_type/creation.py
+++ bothub/project/usecases/template_type/creation.py
@@ -4,10 +4,10 @@ from bothub.project.models import ProjectIntelligence, TemplateType
 class TemplateTypeCreationUseCase:
 
     def get_repository_info_by_project_uuid(self, project_uuid):
-        info = {"uuid": []}
+        info = {"repositories": []}
         p_intelligence_queryset = ProjectIntelligence.objects.filter(project__uuid=project_uuid)
         for project_intelligence in p_intelligence_queryset:
-            info["uuid"].append(project_intelligence.repository.uuid)
+            info["repositories"].append(project_intelligence.repository.uuid)
         return info
 
     def create_template_type(self, template_type_dto: TemplateTypeDTO):
