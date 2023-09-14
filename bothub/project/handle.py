cac64fdda1a2175912ab251be01f7783b061d973
Jackson Barbosa <jbs@ic.ufal.br>
2023-09-14 18:16:15 -0300

    fix queue name

--- bothub/project/handle.py
+++ bothub/project/handle.py
@@ -4,4 +4,4 @@ from .consumers.project_consumer import ProjectConsumer
 
 
 def handle_consumers(channel: Channel) -> None:
-    channel.basic_consume("projects.artificial-intelligence", callback=ProjectConsumer().handle)
+    channel.basic_consume("artificial-intelligence.projects", callback=ProjectConsumer().handle)
