c860921ac9607b59358e63b7701c5b30f82a8ec2
Jackson Barbosa <jbs@ic.ufal.br>
2023-09-14 17:31:35 -0300

    fix recommendations by PR #832

--- bothub/project/usecases/template_type/creation.py
+++ bothub/project/usecases/template_type/creation.py
@@ -1,17 +1,18 @@
 from .template_type_dto import TemplateTypeDTO
 from bothub.project.models import ProjectIntelligence, TemplateType
 
+
 class TemplateTypeCreationUseCase:
 
-    def get_repository_info_by_project_uuid(self, project_uuid):
-        info = {"uuid": []}
+    def get_setup_by_project_uuid(self, project_uuid):
+        setup = {"repositories": []}
         p_intelligence_queryset = ProjectIntelligence.objects.filter(project__uuid=project_uuid)
         for project_intelligence in p_intelligence_queryset:
-            info["uuid"].append(project_intelligence.repository.uuid)
-        return info
+            setup["repositories"].append({"uuid": str(project_intelligence.repository.uuid)})
+        return setup
 
     def create_template_type(self, template_type_dto: TemplateTypeDTO):
-        setup = self.get_repository_info_by_project_uuid(template_type_dto.project_uuid)
+        setup = self.get_setup_by_project_uuid(template_type_dto.project_uuid)
         template_type, created = TemplateType.objects.get_or_create(uuid=template_type_dto.uuid, defaults=dict(name=template_type_dto.name, setup=setup))
         if not created:
             template_type.setup = setup
