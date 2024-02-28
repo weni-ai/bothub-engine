ea072ec6e22c7e287179fd9d175af4deee8b4b51
Mardone <mardone.dev@gmail.com>
2024-02-28 17:46:40 -0300

    Add: missing files

--- bothub/common/tasks.py
+++ bothub/common/tasks.py
@@ -695,4 +695,4 @@ def send_recent_activity(recent_activity_data):
         exchange="recent-activities.topic",
         routing_key="",
     )
-    print(f"[ intelligencePublisher ] message sent - {recent_activity_data} to recent_activities.topic")
+    print(f"[ intelligencePublisher ] message sent - {recent_activity_data} to recent-activities.topic")
